eed52e79411775542739d67b17c6b912f28f607c
Madhavi Tripathi <tripathimadhavi01@gmail.com>
2025-07-19 11:33:09 +0530

    Additional endpoint and route has been made

--- backend/routes/ticketRoute.js
+++ backend/routes/ticketRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createTicket, getAllTickets, getMyTicket, getSingleTicket, getCountStatusAndPrioritywise, UpdateTicket, deleteTicket } = require("../controllers/TicketController");
+const { createTicket, getAllTickets, getMyTicket, getSingleTicket, getCountStatusAndPrioritywise, UpdateTicket, deleteTicket, AdvancedSearch } = require("../controllers/TicketController");
 const { isAuthenticatedUser } = require("../middleware/auth");
 
 const router = express.Router();
@@ -11,5 +11,7 @@ router.route("/getAll").get(getSingleTicket,isAuthenticatedUser)
 router.route("/getAll").get(getCountStatusAndPrioritywise,isAuthenticatedUser)
 router.route("/updateticket").put(UpdateTicket,isAuthenticatedUser)
 router.route("/getAll").delete(deleteTicket,isAuthenticatedUser)
+router.route("/AdvancedSearch").get(AdvancedSearch,isAuthenticatedUser)
+router.route("/delete").delete(deleteTicket,isAuthenticatedUser)
 
 module.exports = router;
