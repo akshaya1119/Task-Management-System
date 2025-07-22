4e7532b560cbcb6f97d868b5a5b2f2f7d4a1c336
Madhavi Tripathi <tripathimadhavi01@gmail.com>
2025-07-22 11:52:07 +0530

    Login module has been changed, new model userauth is created where we will be storing user password

---
+++ backend/controllers/LoginController.js
@@ -0,0 +1,144 @@
+const catchAsyncError = require("../middleware/catchAsyncError");
+const auth = require("../models/UserAuth")
+const User = require("../models/User")
+
+exports.ChangePassword = catchAsyncError(async (req, res, next) => {
+    const userid = await auth.findById(req.params.id).select("+Password");
+    const isPasswordMatched = await userid.comparePassword(req.body.oldPassword);
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Old password is incorrect", 400));
+    }
+
+    if (req.body.newPassword !== req.body.confirmPassword) {
+        return next(new ErrorHandler("password does not match", 400));
+    }
+    const hashedNewPassword = await bcrypt.hash(req.body.newPassword, 10);
+    auth.Password = hashedNewPassword;
+    await auth.save();
+
+})
+
+// Forgot Password
+exports.forgotPassword = catchAsyncError(async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+        return next(new ErrorHandler("User not found", 404));
+    }
+    const userAuth = await auth.findOne({ UserId: user._id });
+
+    if (!userAuth) {
+        return next(new ErrorHandler("User authentication details not found", 404));
+    }
+    // Get ResetPassword Token
+    const resetToken = userAuth.getResetPasswordToken();
+
+    await userAuth.save({ validateBeforeSave: false });
+
+    const resetPasswordUrl = `${req.protocol}://${req.get(
+        "host"
+    )}/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: `Ecommerce Password Recovery`,
+            message,
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to ${user.email} successfully`,
+        });
+    } catch (error) {
+        userAuth.PasswordResetToken = undefined;
+        userAuth.PasswordResetExpire = undefined;
+
+        await userAuth.save({ validateBeforeSave: false });
+
+        return next(new ErrorHandler(error.message, 500));
+    }
+});
+
+
+exports.resetPassword = catchAsyncError(async (req, res, next) => {
+    // creating token hash
+    const resetPasswordToken = crypto
+        .createHash("sha256")
+        .update(req.params.token)
+        .digest("hex");
+
+    const userAuth = await auth.findOne({
+        PasswordResetToken: resetPasswordToken,
+        PasswordResetExpire: { $gt: Date.now() },
+    });
+
+    if (!userAuth) {
+        return next(
+            new ErrorHandler(
+                "Reset Password Token is invalid or has been expired",
+                400
+            )
+        );
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler("Password does not password", 400));
+    }
+
+    userAuth.Password = await bcrypt.hash(req.body.password, 10);
+
+    // Clear the reset token and expiration
+    userAuth.PasswordResetToken = undefined;
+    userAuth.PasswordResetExpire = undefined;
+
+
+    await userAuth.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Password reset successfully.",
+    });
+});
+
+// Logout User
+exports.logout = catchAsyncError(async (req, res, next) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged Out",
+    });
+});
+
+
+
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // checking if user has given password and email both
+
+    if (!email || !password) {
+        return next(new ErrorHandler("Please Enter Email & Password", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+userId");
+    const userpassword = await auth.findById(user).select("+password")
+
+    if (!user) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    const isPasswordMatched = await userpassword.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid email or password", 401));
+    }
+
+    sendToken(user, 200, res);
+});
--- backend/models/UserAuth.js
+++ backend/models/UserAuth.js
@@ -8,12 +8,27 @@ const UserAuthSchema = new mongoose.Schema({
     },
     Password: {
         type: String,
-        required: true
+        required: [true, "Please Enter your password"],
+        minLength: [8, "Password should be greater than 8 characters"],
     },
-    AutogenPass :{
-        type : Boolean,
+    AutogenPass: {
+        type: Boolean,
         default: false
-    }
+    },
+    PasswordResetToken: String,      // Field to store reset token
+    PasswordResetExpire: Date,
 })
 
-mongoose.exports = mongoose.models("UserAuth",UserAuthSchema)
+UserAuthSchema.methods.getResetPasswordToken = function () {
+    const resetToken = crypto.randomBytes(20).toString("hex");
+    //basically neeche ki line ye kh rhi ki tmhe kya update kena h which is token and hex is used for making it readable
+    this.PasswordResetToken = crypto.createHash("sha256").update(token).digest("hex");
+    this.PasswordResetExpire = Date.now() + 60 * 60 * 1000;
+    return resetToken;
+}
+
+UserAuthSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+mongoose.exports = mongoose.model("UserAuth", UserAuthSchema)
